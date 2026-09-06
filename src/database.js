498d7378f0e84c129bb40f248835a980ab6cb829
TalhaBhaiYt <TalhaBhaiYt@users.noreply.github.com>
2026-09-06 17:31:34 +0500

    Add Railway deployment config

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const path  = require('path');
 const bcrypt = require('bcryptjs');
 const fs    = require('fs');
 
-const dataDir = path.join(__dirname, '..', 'data');
+const dataDir = process.env.DATA_DIR || path.join(__dirname, '..', 'data');
 if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir, { recursive: true });
 
 const db = new DatabaseSync(path.join(dataDir, 'kyrex.db'));
